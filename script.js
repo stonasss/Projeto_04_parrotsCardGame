c2465b9690c3ea8d808f589c468d662f8aaf94b2
Luis Bernardo <102603174+stonasss@users.noreply.github.com>
2022-10-20 14:31:18 -0300

    mudanças nos nomes dos arquivos, reajustes ao js/css, esboço da função de virar carta

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-function loadGame(){
-    const choice = Number (prompt ('Entre 4 a 14, somente pares, quantas cartas você quer?'));
-    const deckCards = document.querySelector('.deck');
-    const cardAmount = [choice];
+let deckCards = [];
+let element = document.querySelector(".card");
+const parrotGifs = ['1.gif', '2.gif', '3.gif', '4.gif', '5.gif', '6.gif', '7.gif'];
+const deckDiv = document.querySelector('.deck');
 
-    if (choice < 4 || choice > 14 || choice % 2 !== 0){
+
+function loadGame(){
+    const cardNumber = Number (prompt ('Entre 4 a 14, somente pares, quantas cartas você quer?'));
+
+    if (cardNumber < 4 || cardNumber > 14 || cardNumber % 2 !== 0){
         return loadGame()
     } else {
         i = 0
-        while (i < cardAmount) {
-        
-        deckCards.innerHTML += 
-        `<div class="card">
-            <img src="/Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png">
-        </div>`
+        while (i < cardNumber/2) {
+        deckCards.push(parrotGifs[i])
+        deckCards.push(parrotGifs[i])
         i++
+        }
+    }
+    loadCards()
+}
+
+function loadCards(){
+    for (let i = 0; i < deckCards.length; i++){
+        deckDiv.innerHTML +=
+            `<div onclick="flipCard(this)" class="card">
+                <div class="front"><img src="./assets/${deckCards[i]}"></div>
+                <div class="back"><img src="./assets/back.png"></div>
+            </div>`
     }
 }
+
+function flipCard(element){
+    element.classList.add("flip");
 }
